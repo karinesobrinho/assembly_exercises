8fad6e08829f59fb7d54c17de72eeaff8cfaf4d0
Karine da Silva Sobrinho <karinedasilvasobrinho@MacBook-Air-de-Administrador.local>
2022-10-15 20:12:36 -0300

    reading string

---
+++ str_balance.s
@@ -0,0 +1,47 @@
+#Escreva um programa que detecte se uma string com uma sequência de parênteses 
+#está balanceada ou não. 
+#Uma sequência de parênteses é dita balanceada se, para todo “)“, há 
+#um “(” correspondente que apareceu antes na sequência. 
+#Caso a sequência esteja balanceada, o programa deve imprimir o texto “Balanceada” 
+#seguido de um fim de linha. 
+#Caso contrário, deve imprimir o texto “Desbalanceada” seguido de um fim de linha. 
+#As strings de entrada são compostas apenas dos caracteres “(“, “)”, e “.”. 
+#O caracter “.” indica o fim da sequência.
+
+#Primeiro exemplo de entrada:
+#()(())((())).
+#Primeiro exemplo de saída:
+#Balanceada
+#Segundo exemplo de entrada:
+#(()(()).
+#Segundo exemplo de saída:
+#Desbalanceada
+
+.data
+str1: .asciiz "“Balanceada\n"
+str2: .asciiz "Desbalanceada\n"
+.text
+
+main:
+    move $t0, $zero #conta (
+    move $t1, $zero #conta )
+    move $t3, $zero
+
+    loop: 
+        beq $t3, '.', saiLoop #quando tiverem o mesmo valor sai do loop
+
+        beq $t3, '(', inc_abre
+        beq $t3, ')', inc_fecha
+
+        addi $v0, $zero, 12 #chamada de escrita 
+        syscall
+        add $t3, $v0, $zero #armazena numero escrito no vetor
+        j loop #volta no comeco do loop
+    
+    inc_abre:
+
+    inc_fecha:
+
+    saiLoop: #codigo para encerrar o programa 
+        li $v0,10
+        syscall
