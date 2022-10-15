02b238e8480205b40f8c10eaced5ee52eb3e440f
Karine da Silva Sobrinho <karinedasilvasobrinho@MacBook-Air-de-Administrador.local>
2022-10-15 16:19:34 -0300

    description of the ex

---
+++ pile_numbers.s
@@ -0,0 +1,34 @@
+#Escreva um programa que implemente uma pilha de inteiros com até 10 posições em vetor.  
+#O programa deve ler os seguintes comandos (um por linha) e executá-los:
+
+#eX: empilha o valor inteiro X;
+
+#d: desempilha o último valor (não precisa fazer nada com o valor em si);
+
+#i: imprime o conteúdo da pilha na ordem em que os itens foram empilhados
+
+#s: termina o programa.
+
+
+#No comando i, imprima sempre um espaço depois de cada item. 
+#Depois de imprimir todos, imprima um fim de linha. 
+#Ainda sobre o comando i, se a pilha estiver vazia, não faça nada.
+
+
+#Exemplo de entrada:
+#e1
+#e2
+#e3
+#e4
+#i
+#d
+#i
+#d
+#e5
+#i
+#s
+#Exemplo de saída:
+#1 2 3 4 
+#1 2 3 
+#1 2 5 
+
