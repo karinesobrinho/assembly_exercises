7b483ce498685860ecfb815081791e02abf427bc
Karine da Silva Sobrinho <karinedasilvasobrinho@MacBook-Air-de-Administrador.local>
2022-10-15 16:14:56 -0300

    exercise explaned

--- atv_reader.s
+++ atv_reader.s
@@ -1,3 +1,27 @@
+#Escreva um programa que leia 12 valores inteiros. 
+#Os 10 primeiros devem ser armazenados em um vetor de 10 posições 
+#(cada posição com o tamanho de uma palavra). 
+#Seja V o vetor, A o décimo primeiro valor lido, e B o décimo segundo valor lido. 
+#Imprima todo elemento E do vetor V que respeite a condição A ≤ E ≤ B. 
+#Imprima um espaço logo depois de cada elemento. 
+#A ordem dos valores impressos deve ser a mesma ordem em que estão armazenados no vetor V.
+
+#Exemplo de entrada:
+#1
+#2
+#3
+#4
+#6
+#5
+#7
+#8
+#9
+#10
+#4
+#7
+#Exemplo de saída:
+#4 6 5 7 ‎ 
+
 .data
     myArray:
     .space 40 #array de 10 posicoes
