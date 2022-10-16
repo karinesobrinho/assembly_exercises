ff6f6c7e1a4e9a8cc0351333d4a62b0e362a6164
Karine da Silva Sobrinho <karinedasilvasobrinho@MacBook-Air-de-Administrador.local>
2022-10-15 21:25:27 -0300

    comments ok

--- str_balance.s
+++ str_balance.s
@@ -25,49 +25,49 @@ str2: .asciiz "Desbalanceada\n"
 main:
     move $t0, $zero #conta (
     move $t1, $zero #conta )
-    move $t3, $zero
+    move $t3, $zero #armazena caracter do input
 
     loop: 
         addi $v0, $zero, 12 #chamada de escrita 
         syscall
-        add $t3, $v0, $zero #armazena numero escrito no vetor
+        add $t3, $v0, $zero #armazena caracter escrito em $t3
         
         beq $t3, '.', saiLoop #quando tiverem o mesmo valor sai do loop
 
-        beq $t3, '(', inc_abre
-        beq $t3, ')', inc_fecha
+        beq $t3, '(', inc_abre #quando tiverem o mesmo valor vai para inc_abre
+        beq $t3, ')', inc_fecha #quando tiverem o mesmo valor vai para inc_fecha
         j loop #volta no comeco do loop
 
     inc_abre:
-        addi $t0, 1
-        j loop
+        addi $t0, 1 #incrementa contagem de (
+        j loop #volta para o loop de leitura e redirecionamento
 
     inc_fecha:
-        addi $t1, 1
+        addi $t1, 1 #incrementa contagem de )
 
-        slt $t4, $t0, $t1 #se $t0 for menor do que $t1 $t4 recebe 1
+        slt $t4, $t0, $t1 #se $t0 for menor do que $t1 $t4 recebe 1, ja esta desbalanceda
         
-        j loop
+        j loop #volta para o loop de leitura e redirecionamento
 
     print_bal:
         addi $v0, $zero, 4 # codigo syscall print_str
-        la $a0, str1 # primeira string a ser exibida
+        la $a0, str1 # primeira Balanceada a ser exibida
         syscall
-        j saiPrograma
+        j saiPrograma #pula para encerrar o programa
 
     print_des:
         addi $v0, $zero, 4 # codigo syscall print_str
-        la $a0, str2 # primeira string a ser exibida
+        la $a0, str2 # primeira Desbalanceada a ser exibida
         syscall
-        j saiPrograma
+        j saiPrograma #pula para encerrar o programa
 
-    saiLoop: #codigo para encerrar o programa 
-        beq $t4, 1, print_des
+    saiLoop: 
+        beq $t4, 1, print_des #se $t4 recebeu 1 entao ja estava desbalanceada
 
-        beq $t0, $t1, print_bal
+        beq $t0, $t1, print_bal #se o numero de abre e fecha parenteses é igual e $t4 !=0 entao esta banceada
 
-        j print_des
+        j print_des #se não eram iguais entao esta desbalanceada
 
-    saiPrograma:
+    saiPrograma: #encerra o programa
         li $v0,10
         syscall
